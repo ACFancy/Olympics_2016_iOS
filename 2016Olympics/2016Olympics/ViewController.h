8a764c12152a666463e9146e690d339fa1a81f46
ACFancy <pgwizardlv@gmail.com>
2015-04-30 09:55:57 +0800

    Second commit

---
+++ 2016Olympics/2016Olympics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2016Olympics
+//
+//  Created by ianc on 15/4/29.
+//  Copyright (c) 2015年 PGWizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2016Olympics/2016Olympics/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2016Olympics
+//
+//  Created by ianc on 15/4/29.
+//  Copyright (c) 2015年 PGWizard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

836cd74d64beb8a9aa124b9c2f24da187ab3c47e
ACFancy <pgwizardlv@gmail.com>
2015-05-13 01:55:09 +0800

    add some

---
+++ 2016Olympics/ScheduleBL.h
@@ -0,0 +1,15 @@
+//
+//  ScheduleBL.h
+//  BusinessLogicLayer
+//
+//  Created by ianc on 15/5/5.
+//  Copyright (c) 2015年 PGWizard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScheduleBL : NSObject
+
+- (NSMutableDictionary *)readData;
+
+@end
